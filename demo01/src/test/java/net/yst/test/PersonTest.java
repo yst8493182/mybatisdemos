c5590c69bfd58f5dd32a9babdc664984242a6b11
unknown <000000979@xc.com>
2016-06-30 18:28:52 +0800

    动态sql-查询、插入

--- demo01/src/test/java/net/yst/test/PersonTest.java
+++ demo01/src/test/java/net/yst/test/PersonTest.java
@@ -25,14 +25,12 @@ public class PersonTest {
     }
 
 
-
-
     @Test
-    public void selectById(){
+    public void selectById() {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         Map<String, Object> map = new HashMap<>();
         map.put("id", 2);
-        List<Person> personList=sqlSession.selectList("mappers.PersonMapper.selectById", map);
+        List<Person> personList = sqlSession.selectList("mappers.PersonMapper.selectById", map);
     }
 
     @Test
@@ -45,11 +43,72 @@ public class PersonTest {
         person.setAge(3);
         person.setAddress("盐城大丰");
         person.setBirthday(new Date());
-        int count=sqlSession.insert("mappers.PersonMapper.addPerson", person);
+        int count = sqlSession.insert("mappers.PersonMapper.addPerson", person);
         System.out.println(count);
         sqlSession.commit();
         sqlSession.close();
         System.out.println(person);
+    }
+
+
+    /**
+     * 动态sql 查询
+     */
+    @Test
+    public void dynamicSelect() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Map<String, Object> map = new HashMap<>();
+        map.put("age", 5);
+        List<Person> persons = sqlSession.selectList("mappers.PersonMapper.dynamicSelect", map);
+        sqlSession.close();
+        for (Person p : persons) {
+            System.out.println(p);
+        }
+    }
+
+    /**
+     * 动态sql 查询
+     */
+    @Test
+    public void dynamicSelect1() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Person person = new Person();
+        person.setPid(6);
+        person.setAge(5);
+        List<Person> persons = sqlSession.selectList("mappers.PersonMapper.dynamicSelect1", person);
+        sqlSession.close();
+        for (Person p : persons) {
+            System.out.println(p);
+        }
+    }
+
+
+    /**
+     * 动态sql 插入数据
+     */
+    @Test
+    public void dynamicInsert() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        Map<String, List<Person>> map = new HashMap<>();
+        List<Person> personList = new ArrayList<>();
+        Person person = new Person();
+        map.put("persons", personList);
+        for(int i = 0; i < 200; i++) {
+            person.setPname("测试"+i);
+            person.setAge(i);
+            personList.add(person);
+            if (i % 20 == 0) {
+                sqlSession.insert("mappers.PersonMapper.dynamicInsert", map);
+                personList.clear();
+            }
+        }
+        sqlSession.insert("mappers.PersonMapper.dynamicInsert", map);
+
+
+        sqlSession.commit();
+        sqlSession.close();
+
     }
 
 }
